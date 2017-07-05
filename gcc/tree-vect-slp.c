a3ba4a0362e6707ca4ac86ee8590faecc10ae5d4
Dominik Inführ <dominik.infuehr@theobroma-systems.com>
2017-07-05 15:09:34 +0200

    tree-vect-slp: Enable vectorization when -fnoalias is given.
    GCC 6+ detects more SLP instances compared to earlier versions.
    Cost analysis is done for all instances together and not all
    combinations. Additional instances make vectorization unprofitable,
    fix it by ignoring all but first instance when -fnoalias is given.

--- gcc/tree-vect-slp.c
+++ gcc/tree-vect-slp.c
@@ -2526,6 +2526,18 @@ vect_slp_analyze_operations (vec<slp_instance> slp_instances, void *data)
 	  vect_free_slp_instance (instance);
           slp_instances.ordered_remove (i);
 	}
+      else if (noalias != 0 && i > 0)
+	{
+	  dump_printf_loc (MSG_NOTE, vect_location,
+			   "removing SLP instance operations starting from: ");
+	  dump_gimple_stmt (MSG_NOTE, TDF_SLIM,
+			    SLP_TREE_SCALAR_STMTS
+			      (SLP_INSTANCE_TREE (instance))[0], 0);
+	  dump_printf_loc (MSG_NOTE, vect_location,
+			   "remove all but first instance\n");
+	  vect_free_slp_instance (instance);
+          slp_instances.ordered_remove (i);
+	}
       else
 	{
 	  /* Compute the costs of the SLP instance.  */
