591c00a2639f040c1f7d20adbe7e57be4146b063
Christoph Muellner <christoph.muellner@theobroma-systems.com>
2018-06-13 21:10:15 +0200

    Revert "tree-ssa-cpp: Initalize loop optimizer."
    The initialize loop optimizer patch is confirmed
    to be not needed on GCC 7.3.0.
    
    This reverts commit 8ab36d8103543b2572c34c8824c0c81ef26696a2.

--- gcc/tree-ssa-ccp.c
+++ gcc/tree-ssa-ccp.c
@@ -3121,8 +3121,6 @@ pass_fold_builtins::execute (function *fun)
   basic_block bb;
   unsigned int todoflags = 0;
 
-  loop_optimizer_init (LOOPS_HAVE_SIMPLE_LATCHES | LOOPS_HAVE_PREHEADERS);
-
   FOR_EACH_BB_FN (bb, fun)
     {
       gimple_stmt_iterator i;
